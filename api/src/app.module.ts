ebdb66c1f98d133cea6c846e4638f8d07c1f50f9
Volynskyi-Kirill <kirill108bol@gmail.com>
2024-10-03 18:03:35 +0200

    TS WORK! register resources with prisma

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,21 +1,46 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma/prisma.service';
+
+type AdminJSType = {
+  registerAdapter: (adapter: any) => void;
+};
+
+type AdminModuleType = {
+  createAdminAsync: (options: any) => any;
+};
 
 @Module({
   imports: [
-    import('@adminjs/nestjs').then(({ AdminModule }) =>
-      AdminModule.createAdminAsync({
+    (async () => {
+      const AdminJS: AdminJSType = (await import('adminjs')).default;
+      const { Database, Resource, getModelByName } = await import(
+        '@adminjs/prisma'
+      );
+      const { AdminModule }: { AdminModule: AdminModuleType } = await import(
+        '@adminjs/nestjs'
+      );
+
+      AdminJS.registerAdapter({ Database, Resource });
+      const prisma = new PrismaService();
+
+      return AdminModule.createAdminAsync({
         useFactory: () => ({
           adminJsOptions: {
             rootPath: '/admin',
-            resources: [],
+            resources: [
+              {
+                resource: { model: getModelByName('Employee'), client: prisma },
+                options: {},
+              },
+            ],
           },
         }),
-      }),
-    ),
+      });
+    })(),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
