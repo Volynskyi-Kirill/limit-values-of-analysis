67e67c5055256dc8f2f3d5df847860cefcc4a0c7
Volynskyi-Kirill <kirill108bol@gmail.com>
2024-10-03 21:00:24 +0200

    вынес ресурс в отдельный модуль

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
+import { EmployeeResource } from './resources/employee.resource';
 
 type AdminModuleType = {
   createAdminAsync: (options: any) => any;
@@ -11,37 +12,19 @@ type AdminModuleType = {
   imports: [
     (async () => {
       const AdminJS = (await import('adminjs')).default;
-      const { Database, Resource, getModelByName } = await import(
-        '@adminjs/prisma'
-      );
+      const { Database, Resource } = await import('@adminjs/prisma');
       const { AdminModule }: { AdminModule: AdminModuleType } = await import(
         '@adminjs/nestjs'
       );
 
       AdminJS.registerAdapter({ Database, Resource });
-      const prisma = new PrismaService();
+      const employeeResource = await EmployeeResource();
 
       return AdminModule.createAdminAsync({
         useFactory: () => ({
           adminJsOptions: {
             rootPath: '/admin',
-            resources: [
-              {
-                resource: { model: getModelByName('Employee'), client: prisma },
-                options: {
-                  properties: {
-                    role: {
-                      type: 'string',
-                      availableValues: [
-                        { value: 'SUPER_ADMIN', label: 'супер адмін' },
-                        { value: 'ADMIN', label: 'адмін' },
-                        { value: 'MED_WORKER', label: 'мед працівник' },
-                      ],
-                    },
-                  },
-                },
-              },
-            ],
+            resources: [employeeResource],
           },
         }),
       });
