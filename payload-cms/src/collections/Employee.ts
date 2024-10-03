142eadf6398eb828ce2c56a1b9512e0e60e2242c
Volynskyi-Kirill <kirill108bol@gmail.com>
2024-10-03 16:50:51 +0200

    try to use payload - suck

---
+++ payload-cms/src/collections/Employee.ts
@@ -0,0 +1,15 @@
+import { CollectionConfig } from 'payload/types';
+
+const Employee: CollectionConfig = {
+  slug: 'Employee',
+  // auth: true,
+  // admin: {
+  //   useAsTitle: 'email',
+  // },
+  fields: [
+    // Email added by default
+    // Add more fields as needed
+  ],
+};
+
+export default Employee;
