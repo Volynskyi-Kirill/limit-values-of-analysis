ed8fd0699e9a255ec9d14bd4dc970aacde827cd0
Volynskyi-Kirill <kirill108bol@gmail.com>
2024-10-09 20:56:06 +0200

    ref save role from custom component

--- api/src/adminjs/components/role-select.tsx
+++ api/src/adminjs/components/role-select.tsx
@@ -7,21 +7,14 @@ import { Select } from '@adminjs/design-system';
 import { getAvailableRoles } from '../lib/helpers';
 
 const RoleSelect: React.FC = (props: any) => {
-  console.log('props: ', props);
-  const { record, onChange } = props;
-  console.log('onChange: ', onChange);
-  console.log('record: ', record);
+  const { onChange } = props;
   const [selectedRole, setSelectedRole] = useState<string | undefined>(
     undefined,
   );
-  console.log('selectedRole: ', selectedRole);
-
   const [currentAdmin, setCurrentAdmin] = useCurrentAdmin();
 
   const currentRole = currentAdmin?.role;
-
   const options = getAvailableRoles(currentRole);
-  console.log('options: ', options);
 
   const handleRoleChange = (selected: any) => {
     setSelectedRole(selected);
