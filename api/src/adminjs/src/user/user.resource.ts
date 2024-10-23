f0d072abd6bde1acb91db4a4438909fc8c2b4c87
Volynskyi-Kirill <kirill108bol@gmail.com>
2024-10-23 18:30:32 +0200

    chor: запретил удаления в админДЖС

--- api/src/adminjs/src/user/user.resource.ts
+++ api/src/adminjs/src/user/user.resource.ts
@@ -1,6 +1,10 @@
 import { prismaAdminJSClient } from 'src/modules/adminjs.module';
 import { loadComponents } from '../../components/components';
-import { handleAfterNewUser, handleBeforeEditUser, handleBeforeNewUser } from './user.handler';
+import {
+  handleAfterNewUser,
+  handleBeforeEditUser,
+  handleBeforeNewUser,
+} from './user.handler';
 import { DEFAULT_CREATED_BY_OPTION } from 'src/adminjs/shared/options';
 import { handleUpdatedAt } from 'src/adminjs/shared/handlers';
 import { AuthService } from 'src/auth/auth.service';
@@ -33,6 +37,9 @@ export const UserResource = async (
         },
         edit: {
           before: [handleUpdatedAt, handleBeforeEditUser],
+        },
+        delete: {
+          isAccessible: false,
         },
       },
       properties: {
